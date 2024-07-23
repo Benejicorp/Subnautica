1dc1ee4ff074d2b46c75e4d4b6fa82e6e28d03f0
Benejico <147986694+Benejico@users.noreply.github.com>
2024-07-23 04:34:13 +0200

    Update data.js

--- data.js
+++ data.js
@@ -9,19 +9,19 @@ const data = {
                         { "text": "Les pieds mouillés - Plongez pour la première fois", "tags": [] },
                         { "text": "Installation sur le long terme - Construire un habitat", "tags": [] },
                         { "text": "Extinction évitée - Réparer le réacteur de l'Aurora", "tags": [] },
-                        { "text": "Technologies Anciennes - Trouver la plateforme armée", "tags": [] }
-                        { "text": "Propulsion Personnelle - Construire un Seamoth", "tags": [] }
-                        { "text": "Commandé le Prawn - Construire une combinaison Prawn", "tags": [] }
-                        { "text": "Résidence en bord de mer avec vue sur l'océan - Trouver l'habitat Degasi sur l'ile flottante", "tags": [] }
-                        { "text": "Suivre le Degasi - Trouver l'habitat Degasi dans les grottes des champi-gelées", "tags": [] }
-                        { "text": "Monstre des mers - Trouver la base du Degasi dans les profondeurs des grands récifs", "tags": [] }
-                        { "text": "Meilleur ami de l'homme - Faites éclore un calineur et relachez-le", "tags": [] }
-                        { "text": "Un sous-marin de 40 pieds pour la une - Construire un cyclops", "tags": [] }
-                        { "text": "Suivre la rivière perdue - Trouver le Centre de Recherche Alien", "tags": [] }
-                        { "text": "Activité thermique - Trouver la centrale thermique", "tags": [] }
-                        { "text": "Vingt milles lieues sous les mers - Trouver l'installation de confinement primaire", "tags": [] }
-                        { "text": "Santé optimale - Soigner l'infection", "tags": [] }
-                        { "text": "Laisser seulement les capsules temporelles - Créer et déployer une capsule temporelle", "tags": [] }
+                        { "text": "Technologies Anciennes - Trouver la plateforme armée", "tags": [] },
+                        { "text": "Propulsion Personnelle - Construire un Seamoth", "tags": [] },
+                        { "text": "Commandé le Prawn - Construire une combinaison Prawn", "tags": [] },
+                        { "text": "Résidence en bord de mer avec vue sur l'océan - Trouver l'habitat Degasi sur l'ile flottante", "tags": [] },
+                        { "text": "Suivre le Degasi - Trouver l'habitat Degasi dans les grottes des champi-gelées", "tags": [] },
+                        { "text": "Monstre des mers - Trouver la base du Degasi dans les profondeurs des grands récifs", "tags": [] },
+                        { "text": "Meilleur ami de l'homme - Faites éclore un calineur et relachez-le", "tags": [] },
+                        { "text": "Un sous-marin de 40 pieds pour la une - Construire un cyclops", "tags": [] },
+                        { "text": "Suivre la rivière perdue - Trouver le Centre de Recherche Alien", "tags": [] },
+                        { "text": "Activité thermique - Trouver la centrale thermique", "tags": [] },
+                        { "text": "Vingt milles lieues sous les mers - Trouver l'installation de confinement primaire", "tags": [] },
+                        { "text": "Santé optimale - Soigner l'infection", "tags": [] },
+                        { "text": "Laisser seulement les capsules temporelles - Créer et déployer une capsule temporelle", "tags": [] },
                         { "text": "Filez vers les étoiles - Lancez la fusée Neptune", "tags": [] }
                     ]
                 }
